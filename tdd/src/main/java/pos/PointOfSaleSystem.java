aa436a1878703c00a594ab19a6932ab71ec691a0
Ashkrit <ashkrit@gmail.com>
2019-12-02 20:14:58 +0530

    Support price lookup

--- tdd/src/main/java/pos/PointOfSaleSystem.java
+++ tdd/src/main/java/pos/PointOfSaleSystem.java
@@ -1,11 +1,24 @@
 package pos;
 
+import java.util.Map;
+
 public class PointOfSaleSystem {
+    private final Map<String, Double> productPrice;
+    private Double currentProduct;
+
+    public PointOfSaleSystem(Map<String, Double> productPrice) {
+        this.productPrice = productPrice;
+    }
+
     public String message() {
         return "$10";
     }
 
     public void onBarCode(String barCode) {
+        this.currentProduct = productPrice.get(barCode);
+    }
 
+    public String displayMessage() {
+        return String.format("$%.2f", currentProduct);
     }
 }
--- tdd/src/test/java/pos/PointOfSaleSystemTest.java
+++ tdd/src/test/java/pos/PointOfSaleSystemTest.java
@@ -2,14 +2,33 @@ package pos;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PointOfSaleSystemTest {
 
     @Test
     public void return_message_to_display() {
-        PointOfSaleSystem pos = new PointOfSaleSystem();
+        PointOfSaleSystem pos = new PointOfSaleSystem(new HashMap<>());
         pos.onBarCode("1000000");
         assertEquals("$10", pos.message());
+    }
+
+
+    @Test
+    public void handles_multiple_scans_price() {
+        Map<String, Double> productPrice = new HashMap<>();
+        productPrice.put("1000001", 20.0d);
+        productPrice.put("1000002", 30.0d);
+
+        PointOfSaleSystem pos = new PointOfSaleSystem(productPrice);
+
+        pos.onBarCode("1000001");
+        assertEquals("$20.00", pos.displayMessage());
+
+        pos.onBarCode("1000002");
+        assertEquals("$30.00", pos.displayMessage());
     }
 }
