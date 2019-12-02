ae07a071772264d601f2b1e97b785de941a6b85a
Ashkrit <ashkrit@gmail.com>
2019-12-02 09:07:27 +0530

    MOve all the test to equals

--- tdd/src/test/java/math/FractionsAddTest.java
+++ tdd/src/test/java/math/FractionsAddTest.java
@@ -11,42 +11,40 @@ public class FractionsAddTest {
     public void adds_zero_value() {
 
         Fraction result = new Fraction(0).plus(new Fraction(0));
-        assertEquals(0, result.intValue());
+        assertEquals(new Fraction(0), result);
     }
 
     @Test
     public void add_single_zero_value() {
         Fraction result = new Fraction(2).plus(new Fraction(0));
-        assertEquals(2, result.intValue());
+        assertEquals(new Fraction(2), result);
     }
 
 
     @Test
     public void add_zero_and_non_zero_value() {
         Fraction result = new Fraction(0).plus(new Fraction(5));
-        assertEquals(5, result.intValue());
+        assertEquals(new Fraction(5), result);
     }
 
     @Test
     public void add_both_non_zero_value() {
         Fraction result = new Fraction(10).plus(new Fraction(5));
-        assertEquals(15, result.intValue());
+        assertEquals(new Fraction(15), result);
     }
 
 
     @Test
     public void add_negative_value_test() {
         Fraction result = new Fraction(-4).plus(new Fraction(2));
-        assertEquals(-2, result.intValue());
+        assertEquals(new Fraction(-2), result);
     }
 
     @Test
     public void add_values_with_non_zero_denominator() {
 
         Fraction result = new Fraction(1, 5).plus(new Fraction(2, 5));
-        assertEquals(3, result.getNominator());
-        assertEquals(5, result.getDenominator());
+        assertEquals(new Fraction(3, 5), result);
     }
-
 
 }
