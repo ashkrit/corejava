7e58bd745ce17447ac30d1a5887fdbc6b890e884
Ashkrit <ashkrit@gmail.com>
2019-12-03 13:45:45 +0530

    extract function for validation

--- tdd/src/main/java/pos/MerchantStore.java
+++ tdd/src/main/java/pos/MerchantStore.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public class MerchantStore {
     private final Display display;
-    private Map<String, String> productPrice = new HashMap<String, String>() {
+    private final Map<String, String> productPrice = new HashMap<String, String>() {
         {
             put("1234567", "10.99");
             put("234567", "11.99");
@@ -17,15 +17,18 @@ public class MerchantStore {
     }
 
     public void onBarCode(String barCode) {
-        if (barCode == null || barCode.trim().length() == 0) {
+        if (isNullOrEmpty(barCode)) {
             this.display.setText("invalid scan");
         } else {
-
             if (productPrice.containsKey(barCode)) {
                 this.display.setText(productPrice.get(barCode));
             } else {
                 this.display.setText("product not found " + barCode);
             }
         }
+    }
+
+    private boolean isNullOrEmpty(String barCode) {
+        return barCode == null || barCode.trim().length() == 0;
     }
 }
