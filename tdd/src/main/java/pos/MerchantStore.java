b9626971bed7a0117c10d9c24e480849d1a11576
Ashkrit <ashkrit@gmail.com>
2019-12-03 13:43:06 +0530

    Move to lookup tables

--- tdd/src/main/java/pos/MerchantStore.java
+++ tdd/src/main/java/pos/MerchantStore.java
@@ -1,7 +1,16 @@
 package pos;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MerchantStore {
     private final Display display;
+    private Map<String, String> productPrice = new HashMap<String, String>() {
+        {
+            put("1234567", "10.99");
+            put("234567", "11.99");
+        }
+    };
 
     public MerchantStore(Display display) {
         this.display = display;
@@ -11,10 +20,11 @@ public class MerchantStore {
         if (barCode == null || barCode.trim().length() == 0) {
             this.display.setText("invalid scan");
         } else {
+
             if ("1234567".equals(barCode)) {
-                this.display.setText("10.99");
+                this.display.setText(productPrice.get(barCode));
             } else if ("234567".equals(barCode)) {
-                this.display.setText("11.99");
+                this.display.setText(productPrice.get(barCode));
             } else {
                 this.display.setText("product not found " + barCode);
             }
