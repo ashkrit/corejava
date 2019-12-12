245e6eca171be060aa9e185054ea27c99e26c8e8
Ashkrit <ashkrit@gmail.com>
2019-12-12 13:57:11 +0530

    Implement product catalog interface

--- tdd/src/test/java/sales/ProductCatalogTest.java
+++ tdd/src/test/java/sales/ProductCatalogTest.java
@@ -10,21 +10,21 @@ public class ProductCatalogTest {
 
     @Test
     public void product_found() {
-        InMemoryProductCatalog catalog = makeProductCatalog("P1", 100);
+        ProductCatalog catalog = makeProductCatalog("P1", 100);
         assertEquals(100, catalog.findPrice("P1"));
     }
 
     @Test
     public void product_not_found() {
-        InMemoryProductCatalog catalog = makeEmptyCatalog();
+        ProductCatalog catalog = makeEmptyCatalog();
         assertEquals(null, catalog.findPrice("P1"));
     }
 
-    private InMemoryProductCatalog makeEmptyCatalog() {
+    private ProductCatalog makeEmptyCatalog() {
         return new InMemoryProductCatalog(Collections.EMPTY_MAP);
     }
 
-    private InMemoryProductCatalog makeProductCatalog(String barCode, float price) {
+    private ProductCatalog makeProductCatalog(String barCode, float price) {
         return new InMemoryProductCatalog(Collections.singletonMap(barCode, price));
     }
 }
