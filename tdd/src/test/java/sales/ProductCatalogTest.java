528c551f0aa8162eb85c960e5111ac1a6abfdbd9
Ashkrit <ashkrit@gmail.com>
2019-12-12 13:48:11 +0530

    Add factory function for product catalog

--- tdd/src/test/java/sales/ProductCatalogTest.java
+++ tdd/src/test/java/sales/ProductCatalogTest.java
@@ -10,14 +10,22 @@ public class ProductCatalogTest {
 
     @Test
     public void product_found() {
-
-        InMemoryProductCatalog catalog = new InMemoryProductCatalog(Collections.singletonMap("P1", "100"));
+        InMemoryProductCatalog catalog = makeProductCatalog("P1", "100");
         assertEquals("100", catalog.findPrice("P1"));
     }
 
+
     @Test
     public void product_not_found() {
-        InMemoryProductCatalog catalog = new InMemoryProductCatalog(Collections.EMPTY_MAP);
+        InMemoryProductCatalog catalog = makeEmptyCatalog();
         assertEquals(null, catalog.findPrice("P1"));
+    }
+
+    private InMemoryProductCatalog makeEmptyCatalog() {
+        return new InMemoryProductCatalog(Collections.EMPTY_MAP);
+    }
+
+    private InMemoryProductCatalog makeProductCatalog(String barCode, String price) {
+        return new InMemoryProductCatalog(Collections.singletonMap(barCode, price));
     }
 }
