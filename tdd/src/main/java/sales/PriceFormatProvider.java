aa6b18ca6fef29afb4128255e6d52e4f6eaec0ec
Ashkrit <ashkrit@gmail.com>
2020-01-11 11:24:34 +0800

    Reset branch code

--- tdd/src/main/java/sales/PriceFormatProvider.java
+++
@@ -1,7 +0,0 @@
-package sales;
-
-public class PriceFormatProvider {
-    public static String format(Price price) {
-        return String.format("$%,.2f", price.toDollar());
-    }
-}
--- tdd/src/main/java/sales/catalog/ProductCatalog.java
+++
@@ -1,7 +0,0 @@
-package sales.catalog;
-
-import sales.Price;
-
-public interface ProductCatalog {
-    Price findPrice(String barCode);
-}
