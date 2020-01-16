c556880407cb87b5a223ccc1702330c8b5247a5e
Ashkrit <ashkrit@gmail.com>
2020-01-16 22:36:14 +0800

    Extract function for client handling

--- sockets/src/main/java/socket/SingleThreadBlockingServer.java
+++ sockets/src/main/java/socket/SingleThreadBlockingServer.java
@@ -2,6 +2,7 @@ package socket;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class SingleThreadBlockingServer {
 
@@ -10,18 +11,22 @@ public class SingleThreadBlockingServer {
         while (true) {
             var clientSocket = server.accept();
             System.out.println(String.format("Connected to client %s", clientSocket));
-            try (clientSocket;
-                 var in = clientSocket.getInputStream();
-                 var out = clientSocket.getOutputStream()) {
+            handleClient(clientSocket);
+        }
+    }
 
-                //in.transferTo(out);
-                var b = -1;
-                while ((b = in.read()) != -1) {
-                    out.write(magic(b));
-                }
-            } finally {
-                System.out.println(String.format("Disconnected from client %s", clientSocket));
+    private static void handleClient(Socket clientSocket) throws IOException {
+        try (clientSocket;
+             var in = clientSocket.getInputStream();
+             var out = clientSocket.getOutputStream()) {
+
+            //in.transferTo(out);
+            var b = -1;
+            while ((b = in.read()) != -1) {
+                out.write(magic(b));
             }
+        } finally {
+            System.out.println(String.format("Disconnected from client %s", clientSocket));
         }
     }
 
