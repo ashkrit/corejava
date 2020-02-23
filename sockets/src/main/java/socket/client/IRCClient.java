1a8310308934f425af976f02924268575ca30ef6
Ashkrit <ashkrit@gmail.com>
2020-02-24 01:07:49 +0800

    Add IRC client

--- sockets/src/main/java/socket/client/IRCClient.java
+++ sockets/src/main/java/socket/client/IRCClient.java
@@ -2,18 +2,21 @@ package socket.client;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.Map;
 import java.util.Optional;
 import java.util.Properties;
 import java.util.concurrent.Callable;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.BiConsumer;
+import java.util.function.Predicate;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import static java.util.Map.entry;
 import static java.util.Map.ofEntries;
-import static socket.client.IRCMessageCode.NICK_NAME_USED;
-import static socket.client.IRCMessageCode.PRIVATE_MESSAGE;
-import static socket.client.IRCMessageCode.WELCOME_END;
+import static java.util.concurrent.Executors.newSingleThreadExecutor;
+import static socket.client.IRCMessageCode.*;
 import static socket.functions.SafeFunctions.toUnChecked;
 
 public class IRCClient {
@@ -70,28 +73,46 @@ public class IRCClient {
         };
 
 
-        Executors.newSingleThreadExecutor().submit(task);
+        newSingleThreadExecutor().submit(task);
 
-        new BufferedReader(new InputStreamReader(System.in)).lines().forEach(line -> {
-            write(message.out, String.format("PRIVMSG %s :%s", message.channel, line));
-        });
+        var consoleStream = new BufferedReader(new InputStreamReader(System.in));
+        consoleStream
+                .lines()
+                .forEach(line -> sendMessage(message, line));
 
     }
 
-    private static IRCMessageHandler buildMessageHandlers(IRCMessageContext message, BufferedWriter out) {
-        return new IRCMessageHandler(ofEntries(
-                entry(NICK_NAME_USED.code()::equalsIgnoreCase,
-                        (m, matcher) -> System.out.println(NICK_NAME_USED.nextMessage(m, matcher))),
-                entry(WELCOME_END.code()::equalsIgnoreCase,
-                        (m, matcher) -> write(out, IRCMessageCode.WELCOME_END.nextMessage(message, matcher))),
-                entry(IRCMessageCode.CHANNEL_JOINED.code()::equalsIgnoreCase, (m, matcher) -> {
-                    System.out.println(String.format("Joined %s channel", m.channel));
-                    write(out, IRCMessageCode.CHANNEL_JOINED.nextMessage(m, matcher));
-                }),
-                entry(PRIVATE_MESSAGE.code()::equalsIgnoreCase,
-                        (m, matcher) -> System.out.println(PRIVATE_MESSAGE.nextMessage(m, matcher))
+    private static void sendMessage(IRCMessageContext message, String line) {
+        write(message.out, String.format("PRIVMSG %s :%s", message.channel, line));
+    }
 
-                )));
+    private static IRCMessageHandler buildMessageHandlers(IRCMessageContext message, BufferedWriter out) {
+
+        Map<Predicate<String>, BiConsumer<IRCMessageContext, Matcher>> handlers = ofEntries(
+                entry(NICK_NAME_USED.code()::equalsIgnoreCase, nickNameTaken()),
+                entry(WELCOME_END.code()::equalsIgnoreCase, joinChannel(message, out)),
+                entry(CHANNEL_JOINED.code()::equalsIgnoreCase, (m, matcher) -> channelJoined(out, m, matcher)),
+                entry(PRIVATE_MESSAGE.code()::equalsIgnoreCase, showPrivateMessage()));
+
+
+        return new IRCMessageHandler(handlers);
+    }
+
+    private static BiConsumer<IRCMessageContext, Matcher> showPrivateMessage() {
+        return (m, matcher) -> System.out.println(PRIVATE_MESSAGE.nextMessage(m, matcher));
+    }
+
+    private static void channelJoined(BufferedWriter out, IRCMessageContext m, Matcher matcher) {
+        System.out.println(String.format("Joined %s channel", m.channel));
+        write(out, CHANNEL_JOINED.nextMessage(m, matcher));
+    }
+
+    private static BiConsumer<IRCMessageContext, Matcher> joinChannel(IRCMessageContext message, BufferedWriter out) {
+        return (m, matcher) -> write(out, IRCMessageCode.WELCOME_END.nextMessage(message, matcher));
+    }
+
+    private static BiConsumer<IRCMessageContext, Matcher> nickNameTaken() {
+        return (m, matcher) -> System.out.println(NICK_NAME_USED.nextMessage(m, matcher));
     }
 
     private static void processAction(BufferedWriter out, AtomicBoolean handshakeDone,
