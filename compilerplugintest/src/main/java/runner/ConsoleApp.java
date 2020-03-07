15ff57c372c38d567b25d08fa6d17e48d989a962
Ashkrit <ashkrit@gmail.com>
2020-03-07 23:28:36 +0800

    Skeleton project for Maven plugin test

---
+++ compilerplugintest/src/main/java/runner/ConsoleApp.java
@@ -0,0 +1,8 @@
+package runner;
+
+public class ConsoleApp {
+
+    public static void main(String[] args) {
+        System.out.println("Say Hi" );
+    }
+}
---
+++ compilerplugintest/src/test/java/runner/ConsoleAppTest.java
@@ -0,0 +1,12 @@
+package runner;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ConsoleAppTest {
+    @Test
+    public void sayHello() {
+
+        Assertions.assertEquals(true, true);
+    }
+}
