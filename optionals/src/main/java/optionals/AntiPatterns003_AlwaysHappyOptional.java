20de7b30d4150d2f2cb8251798e98b24c21da537
Ashkrit <ashkrit@gmail.com>
2020-03-14 12:02:44 +0800

    Fix always happy optional

--- optionals/src/main/java/optionals/AntiPatterns003_AlwaysHappyOptional.java
+++ optionals/src/main/java/optionals/AntiPatterns003_AlwaysHappyOptional.java
@@ -12,8 +12,13 @@ public class AntiPatterns003_AlwaysHappyOptional {
         Optional<String> phone = contactNumber(p);
         Optional<String> email = email(p);
 
+        //Don't do this
         System.out.println("Calling Phone " + phone.get());
         System.out.println("Sending Email " + email.get());
+
+        //Use ifPresent to avoid runtime error
+        phone.ifPresent(contact -> System.out.println("Sending email to " + contact));
+        email.ifPresent(contact -> System.out.println("Calling " + contact));
     }
 
     private static Optional<String> email(Person p) {
