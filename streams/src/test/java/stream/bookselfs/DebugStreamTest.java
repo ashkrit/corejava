7d76e6f994abf3abedc2f16c4ad8695bbc3ec718
Ashkrit <ashkrit@gmail.com>
2020-09-08 10:39:41 +0800

    Debugging using peek

---
+++ streams/src/test/java/stream/bookselfs/DebugStreamTest.java
@@ -0,0 +1,30 @@
+package stream.bookselfs;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class DebugStreamTest {
+
+    @Test
+    public void debug_using_peek() {
+        List<Integer> values = Arrays.asList(1, 4, 5, 6);
+
+        AtomicLong itemCount = new AtomicLong(0);// Used only for mutation
+        int total = values
+                .stream()
+                .mapToInt(v -> v)
+                .peek(x -> {
+                    itemCount.incrementAndGet();
+                    System.out.println("Received " + x);
+                }) // Debug hook for streams
+                .sum();
+
+        Assertions.assertEquals(4, itemCount.get());
+
+
+    }
+}
