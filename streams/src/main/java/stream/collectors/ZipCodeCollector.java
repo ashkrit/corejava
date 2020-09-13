5ff14227c53fc1aa92cb5f056494ded6bb41146a
Ashkrit <ashkrit@gmail.com>
2020-09-13 14:18:52 +0800

    Extract zip code collector in new class

---
+++ streams/src/main/java/stream/collectors/ZipCodeCollector.java
@@ -0,0 +1,81 @@
+package stream.collectors;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+
+public class ZipCodeCollector {
+
+    static Supplier<List<List<Store>>> supplier = () -> {
+        List<List<Store>> initial = new ArrayList<>();
+        initial.add(new ArrayList<>());
+        return initial;
+    };
+
+    static BiConsumer<List<List<Store>>, Store> accumulator = (buffer, value) -> {
+        int total = buffer.size();
+        List<Store> lastGroup = buffer.get(total - 1);
+
+        if (!lastGroup.isEmpty()) {
+            Store lastStore = lastGroup.get(lastGroup.size() - 1);
+            if (value.distance(lastStore) > 2) {
+                List<Store> newGroup = new ArrayList<>();
+                newGroup.add(value);
+                buffer.add(newGroup);
+                return;
+            }
+        }
+        lastGroup.add(value);
+    };
+
+    static BinaryOperator<List<List<Store>>> combiner = (first, second) -> {
+
+        if (first.isEmpty()) return second;
+        if (second.isEmpty()) return first;
+
+        List<Store> secondGroup = second.get(0);
+        Store secondGroupFirst = secondGroup.get(0);
+
+        List<Store> firstGroup = first.get(first.size() - 1);
+        Store firstGroupLast = firstGroup.get(firstGroup.size() - 1);
+
+        if (firstGroupLast.distance(secondGroupFirst) <= 2) {
+            firstGroup.addAll(secondGroup);
+            second.remove(secondGroup);
+        }
+
+        first.addAll(second);
+        return first;
+    };
+
+
+    public static Collector<Store, List<List<Store>>, List<List<Store>>> create() {
+        return Collector.of(supplier, accumulator, combiner);
+    }
+
+    public static class Store {
+        public final int position;
+        public final String name;
+
+        Store(String name, int position) {
+            this.position = position;
+            this.name = name;
+        }
+
+        public static Store building(String name, int position) {
+            return new Store(name, position);
+        }
+
+        public int distance(Store lastStore) {
+            return Math.abs(position - lastStore.position);
+        }
+
+        @Override
+        public String toString() {
+            return name + "(" + position + ")";
+        }
+    }
+}
--- streams/src/test/java/stream/bookselfs/custom/ZipCodeAllocationTest.java
+++ streams/src/test/java/stream/bookselfs/custom/ZipCodeAllocationTest.java
@@ -1,45 +1,21 @@
 package stream.bookselfs.custom;
 
 import org.junit.jupiter.api.Test;
+import stream.collectors.ZipCodeCollector;
+import stream.collectors.ZipCodeCollector.Store;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
-import java.util.function.BiConsumer;
-import java.util.function.BinaryOperator;
-import java.util.function.Supplier;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertIterableEquals;
-import static stream.bookselfs.custom.ZipCodeAllocationTest.Store.building;
+import static stream.collectors.ZipCodeCollector.Store.building;
 
 public class ZipCodeAllocationTest {
 
-    static class Store {
-        public final int position;
-        public final String name;
-
-        Store(String name, int position) {
-            this.position = position;
-            this.name = name;
-        }
-
-        static Store building(String name, int position) {
-            return new Store(name, position);
-        }
-
-        public int distance(Store lastStore) {
-            return Math.abs(position - lastStore.position);
-        }
-
-        @Override
-        public String toString() {
-            return name + "(" + position + ")";
-        }
-    }
 
     @Test
     public void generatePostalCode() {
@@ -60,10 +36,12 @@ public class ZipCodeAllocationTest {
                 building("S7", 117));
 
 
-        Stream<Store> sortedByDistance = stores.stream().sorted(Comparator.comparingLong(x -> x.position));
+        Stream<Store> sortedByDistance = stores
+                .stream()
+                .sorted(Comparator.comparingLong(x -> x.position));
 
         List<List<String>> q =
-                sortedByDistance.collect(Collector.of(supplier, accumulator, combiner))
+                sortedByDistance.collect(ZipCodeCollector.create())
                         .stream()
                         .map(x -> x.stream().map(b -> b.name).collect(Collectors.toList()))
                         .collect(Collectors.toList());
@@ -74,46 +52,4 @@ public class ZipCodeAllocationTest {
         assertIterableEquals(Arrays.asList("S3", "S4"), q.get(index++));
         assertIterableEquals(Arrays.asList("S5", "S6", "S7"), q.get(index++));
     }
-
-    Supplier<List<List<Store>>> supplier = () -> {
-        List<List<Store>> initial = new ArrayList<>();
-        initial.add(new ArrayList<>());
-        return initial;
-    };
-
-    BiConsumer<List<List<Store>>, Store> accumulator = (buffer, value) -> {
-        int total = buffer.size();
-        List<Store> lastGroup = buffer.get(total - 1);
-
-        if (!lastGroup.isEmpty()) {
-            Store lastStore = lastGroup.get(lastGroup.size() - 1);
-            if (value.distance(lastStore) > 2) {
-                List<Store> newGroup = new ArrayList<>();
-                newGroup.add(value);
-                buffer.add(newGroup);
-                return;
-            }
-        }
-        lastGroup.add(value);
-    };
-
-    BinaryOperator<List<List<Store>>> combiner = (first, second) -> {
-
-        if (first.isEmpty()) return second;
-        if (second.isEmpty()) return first;
-
-        List<Store> secondGroup = second.get(0);
-        Store secondGroupFirst = secondGroup.get(0);
-
-        List<Store> firstGroup = first.get(first.size() - 1);
-        Store firstGroupLast = firstGroup.get(firstGroup.size() - 1);
-
-        if (firstGroupLast.distance(secondGroupFirst) <= 2) {
-            firstGroup.addAll(secondGroup);
-            second.remove(secondGroup);
-        }
-
-        first.addAll(second);
-        return first;
-    };
 }
