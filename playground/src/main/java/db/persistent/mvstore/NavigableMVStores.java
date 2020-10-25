a8b925236c1d65772e1ccf5d60492f3e1af410ad
Ashkrit <ashkrit@gmail.com>
2020-10-25 10:41:50 +0800

    Refactor: Remove code duplication in MV Store by making predicate as parameter

--- playground/src/main/java/db/persistent/mvstore/NavigableMVStores.java
+++ playground/src/main/java/db/persistent/mvstore/NavigableMVStores.java
@@ -5,8 +5,10 @@ import org.h2.mvstore.Cursor;
 import org.h2.mvstore.MVMap;
 import org.h2.mvstore.MVStore;
 
+import java.util.function.BiFunction;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class NavigableMVStores implements NavigablePersistentStore {
     private final MVStore store;
@@ -29,26 +31,24 @@ public class NavigableMVStores implements NavigablePersistentStore {
 
     @Override
     public <Row_Type> void iterate(String fromKey, Function<byte[], Row_Type> converter, Consumer<Row_Type> consumer, int limit) {
-        Cursor<byte[], byte[]> itr = db.cursor(fromKey.getBytes());
-        int tracker = limit;
-        while (itr.hasNext() && tracker > 0) {
-            String s = new String(itr.getKey());
-            if (!s.startsWith(fromKey)) {
-                break;
-            }
-            consumer.accept(converter.apply(itr.getValue()));
-            itr.next();
-            tracker--;
-        }
+
+        Predicate<String> predicate = v1 -> v1.startsWith(fromKey);
+        match(fromKey, predicate.negate(), converter, consumer, limit);
     }
 
     @Override
     public <Row_Type> void iterate(String fromKey, String toKey, Function<byte[], Row_Type> converter, Consumer<Row_Type> consumer, int limit) {
+        Predicate<String> predicate = v1 -> v1.compareTo(toKey) > 0;
+        match(fromKey, predicate, converter, consumer, limit);
+    }
+
+    private <Row_Type> void match(String fromKey, Predicate<String> predicate, Function<byte[], Row_Type> converter, Consumer<Row_Type> consumer, int limit) {
         Cursor<byte[], byte[]> itr = db.cursor(fromKey.getBytes());
         int tracker = limit;
         while (itr.hasNext() && tracker > 0) {
             String s = new String(itr.getKey());
-            if (s.compareTo(toKey) > 0) {
+
+            if (predicate.test(s)) {
                 break;
             }
             consumer.accept(converter.apply(itr.getValue()));
