bf827a8cd27841a576207dfb4c0bd491b25e4ac3
Ashkrit <ashkrit@gmail.com>
2020-11-22 17:39:33 +0800

    Refactor buffer read/write operation

--- query/src/main/java/query/page/SlotPage.java
+++ query/src/main/java/query/page/SlotPage.java
@@ -2,34 +2,29 @@ package query.page;
 
 import java.nio.ByteBuffer;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 public class SlotPage {
 
-    private final int pageSize;
     private final byte[] data;
     private short version;
     private int pageNumber;
     private int noOfTuple;
-    private final ByteBuffer writer;
+    private final ByteBuffer buffer;
 
     public SlotPage(int pageSize) {
-        this.pageSize = pageSize;
         this.data = new byte[pageSize];
-        writer = ByteBuffer.wrap(data);
+        buffer = ByteBuffer.wrap(data);
     }
 
     public SlotPage(byte[] data) {
-        this.pageSize = data.length;
         this.data = data;
-        this.writer = ByteBuffer.wrap(data);
+        this.buffer = ByteBuffer.wrap(data);
         readHeaders();
     }
 
     private void readHeaders() {
-        version = writer.getShort(PageOffSets.PAGE_VERSION);
-        pageNumber = writer.getInt(PageOffSets.PAGE_NUMBER);
-        noOfTuple = writer.getInt(PageOffSets.NO_OF_TUPLE);
+        version = buffer.getShort(PageOffSets.PAGE_VERSION);
+        pageNumber = buffer.getInt(PageOffSets.PAGE_NUMBER);
+        noOfTuple = buffer.getInt(PageOffSets.NO_OF_TUPLE);
     }
 
     public void version(short version) {
@@ -45,10 +40,14 @@ public class SlotPage {
     }
 
     public byte[] commit() {
-        writer.putShort(PageOffSets.PAGE_VERSION, version);
-        writer.putInt(PageOffSets.PAGE_NUMBER, pageNumber);
-        writer.putInt(PageOffSets.NO_OF_TUPLE, noOfTuple);
+        writeHeaders();
         return data;
+    }
+
+    public void writeHeaders() {
+        buffer.putShort(PageOffSets.PAGE_VERSION, version);
+        buffer.putInt(PageOffSets.PAGE_NUMBER, pageNumber);
+        buffer.putInt(PageOffSets.NO_OF_TUPLE, noOfTuple);
     }
 
     public short version() {
