49613206ee6977691ab4c9ac027f9052aa0b655c
Ashkrit <ashkrit@gmail.com>
2020-11-28 13:51:06 +0800

    Create page index

--- query/src/main/java/query/page/PageIndex.java
+++ query/src/main/java/query/page/PageIndex.java
@@ -1,10 +1,31 @@
 package query.page;
 
-import java.nio.file.Path;
+import query.page.read.ReadPage;
+import query.page.read.ReadableSlottedPage;
+
+import java.util.Map;
+import java.util.TreeMap;
 
 public class PageIndex {
+    private final Map<Integer, byte[]> pageIndex = new TreeMap<>();
+    private final Map<Integer, ReadPage> loadedPage = new TreeMap<>();
 
-    public void insert(int pageId, int offSet, Path location) {
+    public void insert(int pageNumber, byte[] data) {
+        pageIndex.put(pageNumber, data);
+    }
 
+    public int valueAt(int pageNo, int record, byte[] writeBuffer) {
+        ReadPage page = readPage(pageNo);
+        return page.record(record, writeBuffer);
+    }
+
+    public ReadPage readPage(int pageNo) {
+        ReadPage page = loadedPage.get(pageNo);
+        if (page == null) {
+            byte[] rawData = pageIndex.get(pageNo);
+            loadedPage.put(pageNo, new ReadableSlottedPage(rawData));
+        }
+        page = loadedPage.get(pageNo);
+        return page;
     }
 }
--- query/src/test/java/query/page/PageIndexTest.java
+++ query/src/test/java/query/page/PageIndexTest.java
@@ -1,15 +1,41 @@
 package query.page;
 
 import org.junit.jupiter.api.Test;
+import query.page.write.WritableSlotPage;
+import query.page.write.WritePage;
 
-import java.nio.file.Paths;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PageIndexTest {
 
     @Test
-    public void build_page_index() {
+    public void access_record_page_and_index() {
 
         PageIndex index = new PageIndex();
-        index.insert(1, 0, Paths.get("somefile"));
+
+        List<String> novels = asList("Casino Royale",
+                "Live and Let Die",
+                "Moonraker",
+                "Diamonds Are Forever",
+                "From Russia, with Love",
+                "Dr. No",
+                "Goldfinge");
+
+
+        int pageNo = 0;
+        WritePage page = new WritableSlotPage(1024 * 16, (byte) 0, pageNo++, System.currentTimeMillis());
+        novels.forEach(r -> page.write(r.getBytes()));
+        byte[] data = page.commit();
+        System.out.println(page);
+
+        index.insert(page.pageNumber(), data);
+
+        byte[] buffer = new byte[100];
+
+        assertEquals("From Russia, with Love", new String(buffer, 0, index.valueAt(0, 4, buffer)));
+
     }
 }
