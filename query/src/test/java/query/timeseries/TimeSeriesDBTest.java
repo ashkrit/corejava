8c5a72e16693796937c605215c7cd64c8c35e01e
Ashkrit <ashkrit@gmail.com>
2020-12-06 16:41:27 +0800

    Refactor - remove unused reference

--- query/src/test/java/query/timeseries/TimeSeriesDBTest.java
+++ query/src/test/java/query/timeseries/TimeSeriesDBTest.java
@@ -3,17 +3,16 @@ package query.timeseries;
 
 import model.avro.EventInfo;
 import model.avro.LightTaxiRide;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 import query.timeseries.impl.TimeSeriesDBImpl;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.nio.ByteBuffer;
-import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -51,37 +50,8 @@ public class TimeSeriesDBTest {
                 .build();
 
         db.register(LightTaxiRide.class, () -> {
-
             EventIdGenerator generator = new SystemTimeIdGenerator(10_000);
-            AtomicInteger counter = new AtomicInteger();
-            DateTimeFormatter f = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
-
-            Function<Object, EventInfo> fn = row -> {
-
-                LightTaxiRide value = (LightTaxiRide) row;
-
-                String eventId = generator.next(value.getPickupTime());
-
-                Map<CharSequence, Integer> tags = new HashMap<CharSequence, Integer>() {{
-                    put("totalamount", (int) value.getTotalAmount());
-                    put("date", Integer.parseInt(eventId.substring(0, 8)));
-                    put("hour", Integer.parseInt(eventId.substring(8, 10)));
-                }};
-
-
-                return EventInfo
-                        .newBuilder()
-                        .setEventBody(getByteBuffer(value))
-                        .setEventType("TAXIRIDE")
-                        .setEventTime(eventId)
-                        .setHost("NA")
-                        .setService("TAXI-NO")
-                        .setTags(tags)
-                        .build();
-
-
-            };
-            return fn;
+            return toEventInfo(generator);
         });
 
         EventInfo event = db.insert(ride);
@@ -90,6 +60,30 @@ public class TimeSeriesDBTest {
                 () -> assertNotNull(event.getEventTime()),
                 () -> assertEquals(ride, LightTaxiRide.fromByteBuffer(event.getEventBody()))
         );
+    }
+
+    @NotNull
+    private Function<Object, EventInfo> toEventInfo(EventIdGenerator generator) {
+
+        Function<Object, EventInfo> fn = row -> {
+            LightTaxiRide value = (LightTaxiRide) row;
+            String eventId = generator.next(value.getPickupTime());
+            Map<CharSequence, Integer> tags = new HashMap<CharSequence, Integer>() {{
+                put("totalamount", (int) value.getTotalAmount());
+                put("date", Integer.parseInt(eventId.substring(0, 8)));
+                put("hour", Integer.parseInt(eventId.substring(8, 10)));
+            }};
+            return EventInfo
+                    .newBuilder()
+                    .setEventBody(getByteBuffer(value))
+                    .setEventType("TAXIRIDE")
+                    .setEventTime(eventId)
+                    .setHost("NA")
+                    .setService("TAXI-NO")
+                    .setTags(tags)
+                    .build();
+        };
+        return fn;
     }
 
     private ByteBuffer getByteBuffer(LightTaxiRide value) {
