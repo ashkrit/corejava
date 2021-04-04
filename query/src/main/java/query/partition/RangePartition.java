0b44f49e91ba9992a7b848e2eb2ee2cd8e8235eb
Ashkrit <ashkrit@gmail.com>
2021-04-04 14:09:19 +0800

    Verify multiple keys

--- query/src/main/java/query/partition/RangePartition.java
+++ query/src/main/java/query/partition/RangePartition.java
@@ -6,23 +6,48 @@ import java.util.SortedMap;
 import java.util.TreeMap;
 
 public class RangePartition<K extends Comparable, V> {
-    final SortedMap<RangeKey<K>, PartitionValues<K, V>> partitions = new TreeMap<>();
+    final SortedMap<K, PartitionValues<K, V>> partitions = new TreeMap<>();
 
     public void put(K key, V value) {
         if (partitions.isEmpty()) {
             String node = "node" + partitions.size();
             PartitionValues<K, V> values = new PartitionValues<>(node, 10);
             values.put(key, value);
-            partitions.put(values.rangeKey(), values);
+            partitions.put(values.min(), values);
+            return;
+        }
+
+
+        SortedMap<K, PartitionValues<K, V>> tail = partitions.tailMap(key);
+        if (tail.isEmpty()) {
+            K maxKey = partitions.lastKey();
+            PartitionValues<K, V> values = partitions.get(maxKey);
+            values.put(key, value);
+        } else {
+            K minKey = tail.firstKey();
+            PartitionValues<K, V> values = partitions.get(minKey);
+            values.put(key, value);
+            if (key.compareTo(minKey) < 0) {
+                partitions.remove(minKey);
+                partitions.put(values.min(), values);
+            }
         }
 
     }
 
-    public PartitionValues<K, V> get(K key) {
-        if (partitions.size() == 1) {
-            return partitions.get(partitions.lastKey());
+    public PartitionValues<K, V> getPartition(K key) {
+        SortedMap<K, PartitionValues<K, V>> tail = partitions.tailMap(key);
+        if (tail.isEmpty()) {
+            K maxKey = partitions.lastKey();
+            return partitions.get(maxKey);
+        } else {
+            K minKey = tail.firstKey();
+            return partitions.get(minKey);
         }
-        return null;
+    }
+
+    public V getValue(K key) {
+        return getPartition(key).get(key);
     }
 
     static class PartitionValues<K extends Comparable, V> {
@@ -43,25 +68,15 @@ public class RangePartition<K extends Comparable, V> {
             values.put(key, value);
         }
 
-        public RangeKey<K> rangeKey() {
-            return new RangeKey<>(values.firstKey(), values.lastKey());
+        public K min() {
+            return values.firstKey();
+        }
+
+        public K max() {
+            return values.lastKey();
         }
 
     }
 
-    public static class RangeKey<K extends Comparable> implements Comparable<RangeKey> {
-        final K start;
-        final K end;
-
-        RangeKey(K start, K end) {
-            this.start = start;
-            this.end = end;
-        }
-
-        @Override
-        public int compareTo(@NotNull RangeKey o) {
-            return o.start.compareTo(start);
-        }
-    }
 
 }
--- query/src/test/java/query/partition/RangePartitionTest.java
+++ query/src/test/java/query/partition/RangePartitionTest.java
@@ -1,8 +1,8 @@
 package query.partition;
 
 import org.junit.jupiter.api.Test;
-import query.partition.RangePartition.PartitionValues;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RangePartitionTest {
@@ -13,9 +13,26 @@ public class RangePartitionTest {
         RangePartition<Integer, String> range = new RangePartition<>();
 
         range.put(1, "value1");
-        PartitionValues<Integer, String> values = range.get(1);
 
-        assertEquals("value1", values.get(1));
+        assertEquals("value1", range.getValue(1));
+
+    }
+
+
+    @Test
+    public void add_multiple_values() {
+        RangePartition<Integer, String> range = new RangePartition<>();
+
+        range.put(1, "value1");
+        range.put(2, "value2");
+        range.put(10, "value10");
+
+        assertAll(
+                () -> assertEquals("value1", range.getValue(1)),
+                () -> assertEquals("value2", range.getValue(2)),
+                () -> assertEquals("value10", range.getValue(10))
+        );
+
 
     }
 }
