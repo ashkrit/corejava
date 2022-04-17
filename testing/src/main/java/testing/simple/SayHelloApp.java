d961524db9b3f175e825b388bc3b4d6da4922926
Ashkrit <ashkrit@gmail.com>
2022-04-17 13:57:38 +0800

    Testing project samples

---
+++ testing/src/main/java/testing/simple/SayHelloApp.java
@@ -0,0 +1,9 @@
+package testing.simple;
+
+public class SayHelloApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+
+}
--- testing/src/test/java/testing/simple/SayHelloTest.java
+++ testing/src/test/java/testing/simple/SayHelloTest.java
@@ -1,4 +1,4 @@
-package testing.smiple;
+package testing.simple;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
