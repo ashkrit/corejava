32ba7c809578f6efe5f4a852179eb502930f76a5
Ashkrit <ashkrit@gmail.com>
2022-04-17 13:55:10 +0800

    Testing project samples

---
+++ testing/src/test/java/testing/smiple/SayHelloTest.java
@@ -0,0 +1,14 @@
+package testing.smiple;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SayHelloTest {
+
+
+    @Test
+    public void always_passing() {
+        Assertions.assertEquals(10, 10);
+    }
+
+}
