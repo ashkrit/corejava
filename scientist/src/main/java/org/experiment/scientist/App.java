506c14a2a776d5691f2330dd73e15524a48ee426
Ashkrit <ashkrit@gmail.com>
2023-04-08 11:20:41 +0800

    Project Structure

---
+++ scientist/src/main/java/org/experiment/scientist/App.java
@@ -0,0 +1,8 @@
+package org.experiment.scientist;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Say hello");
+    }
+}
---
+++ scientist/src/test/java/org/experiment/scientist/AppTest.java
@@ -0,0 +1,12 @@
+package org.experiment.scientist;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    public void allGood() {
+        Assertions.assertEquals(10, 10);
+    }
+}
