aed967df5ee8a45c0b9bce63490c7bd10617b385
ashkrit@gmail.com <Crack@it12>
2023-06-17 19:00:34 +0800

    Add constants

--- refactoring-patterns/refactoring-patterns/src/main/java/org/refactoring/RefactoredStatementGenerator.java
+++ refactoring-patterns/refactoring-patterns/src/main/java/org/refactoring/RefactoredStatementGenerator.java
@@ -5,6 +5,9 @@ import org.refactoring.Plays.Play;
 
 public class RefactoredStatementGenerator implements StatementGenerator {
 
+    public static final int DEFAULT_AUDIENCE_THRESHHOLD = 30;
+    public static final int COMEDY_AUDIENCE_THRESHHOLD = 20;
+
     @Override
     public String generate(Invoices.Order order, Plays plays) {
 
@@ -37,7 +40,7 @@ public class RefactoredStatementGenerator implements StatementGenerator {
 
     private static double calculateVolumeCredit(Performance performance, Play performancePlay) {
         // add volume calculateAmount
-        double volumeCredit = Math.max(performance.audience - 30, 0);
+        double volumeCredit = Math.max(performance.audience - DEFAULT_AUDIENCE_THRESHHOLD, 0);
         // add extra credit for every ten comedy attendees
         if (PlayType.COMEDY.typeName.equals(performancePlay.type)) {
             volumeCredit += Math.floor(performance.audience / 5);
@@ -51,15 +54,15 @@ public class RefactoredStatementGenerator implements StatementGenerator {
 
             case TRAGEDY: {
                 thisAmount = 40000;
-                if (performance.audience > 30) {
-                    thisAmount += 1000 * (performance.audience - 30);
+                if (performance.audience > DEFAULT_AUDIENCE_THRESHHOLD) {
+                    thisAmount += 1000 * (performance.audience - DEFAULT_AUDIENCE_THRESHHOLD);
                 }
                 break;
             }
             case COMEDY: {
                 thisAmount = 30000;
-                if (performance.audience > 20) {
-                    thisAmount += 10000 + 500 * (performance.audience - 20);
+                if (performance.audience > COMEDY_AUDIENCE_THRESHHOLD) {
+                    thisAmount += 10000 + 500 * (performance.audience - COMEDY_AUDIENCE_THRESHHOLD);
                 }
                 thisAmount += 300 * performance.audience;
                 break;
