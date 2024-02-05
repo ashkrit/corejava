d83aecd3564050c552e74a558da06bef3e5c139b
ashkrit@gmail.com <Crack@it12>
2024-02-05 08:58:16 +0800

    Create Initial project

---
+++ jdbcplus/src/main/java/com/org/jdbcplus/App.java
@@ -0,0 +1,8 @@
+package com.org.jdbcplus;
+
+public class App {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ jdbcplus/src/test/java/com/org/jdbcplus/AppTest.java
@@ -0,0 +1,13 @@
+package com.org.jdbcplus;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+
+    @Test
+    public void test() {
+        Assertions.assertEquals(10,10);
+    }
+}
