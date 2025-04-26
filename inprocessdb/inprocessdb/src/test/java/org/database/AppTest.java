9d8558f4fb24f5c4d32f37e82cd1ba265dcaac1f
ashkrit@gmail.com <ashkrit@gmail.com>
2025-04-26 20:17:32 +0800

    Initial Commit

---
+++ inprocessdb/inprocessdb/src/test/java/org/database/AppTest.java
@@ -0,0 +1,15 @@
+package org.database;
+
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    public void testApp() {
+        // Test case for the main application
+        String[] args = {};
+        App.main(args);
+        // Add assertions to verify the expected behavior
+    }
+}
