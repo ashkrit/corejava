a761e67c14b3843e92c6518f059d5d798b0d8be7
ashkrit@gmail.com <ashkrit@gmail.com>
2026-06-28 18:26:31 +0800

    Add Example of Continuation - With Internal and external counter

--- hpc/hpc/src/main/java/com/org/threads/ContinuationExample.java
+++ hpc/hpc/src/main/java/com/org/threads/ContinuationExample.java
@@ -3,22 +3,31 @@ package com.org.threads;
 import jdk.internal.vm.Continuation;
 import jdk.internal.vm.ContinuationScope;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * Add VM Options
- *
+ * <p>
  * --add-exports java.base/jdk.internal.vm=ALL-UNNAMED --enable-preview
  */
 public class ContinuationExample {
     public static void main(String[] args) {
 
         var scope = new ContinuationScope("main");
+        var counter = new AtomicInteger();
 
         var continuation = new Continuation(scope, () -> {
             System.out.println("Hello from continuation");
+            var x = 0;
+            x++;
+            counter.incrementAndGet();
             Continuation.yield(scope);
-            System.out.println("Hello again from continuation");
+            x++;
+            System.out.println("Hello again from continuation -> " + x + " Counter " + counter.get());
+            x++;
+            counter.incrementAndGet();
             Continuation.yield(scope);
-            System.out.println("Done from continuation");
+            System.out.println("Done from continuation -> " + x + " Counter " + counter.get());
         });
 
         System.out.println("Before starting continuation");
